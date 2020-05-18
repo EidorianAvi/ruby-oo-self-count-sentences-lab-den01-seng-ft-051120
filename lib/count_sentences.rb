00159fc24b45db876c13e452e42c7055069a5599
Rene Avila <reneavila1993@gmail.com>
2020-05-18 22:58:33 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -16,7 +16,7 @@ class String
 
   def count_sentences
     sentence_array =self.split(/[.?!]/)
-    sentence_array.delete_if?
+    sentence_array.delete_if(&:is_empty?)
     sentence_array.size
   end
 end
